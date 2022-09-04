d74d4923f7035d1be46dfac7cc69af50e7123924
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-09-04 18:16:53 -0500

    Modify BladeBit 2.0 Beta for MMX

--- src/plotting/PlotTools.h
+++ src/plotting/PlotTools.h
@@ -7,7 +7,7 @@
 
 #define BB_PLOT_MEMO_MAX_SIZE (48+48+32)
 
-#define BB_PLOT_FILE_LEN_TMP (sizeof( "plot-k32-2021-08-05-18-55-77a011fc20f0003c3adcc739b615041ae56351a22b690fd854ccb6726e5f43b7.plot.tmp" ) - 1)
+#define BB_PLOT_FILE_LEN_TMP (sizeof( "plot-mmx-k32-2077-11-45-14-19-19a011fc20f0003c3adcc739b615041ae56351a22b690fd854ccb6726e5f43b7.plot.tmp" ) - 1)
 #define BB_PLOT_FILE_LEN (BB_PLOT_FILE_LEN_TMP - 4)
 
 struct PlotTools
